1cbf8eb71986ffb6a54e66fc3ae491816e4cfdaa
yasadeeq <88744475+yasadeeq@users.noreply.github.com>
2023-03-14 09:46:08 -0400

    n/a
    FIles for a language game project in Python

---
+++ language_game_project/data.py
@@ -0,0 +1,386 @@
+languages = [
+    {
+        "language": "Mandarin Chinese",
+        "hint_one": "Asia",
+        "hint_two": "Sino-Tibetan",
+        "hint_three": "Nǐ hǎo"
+    },
+    {
+        "language": "Spanish",
+        "hint_one": "Europe",
+        "hint_two": "Indo-European",
+        "hint_three": "Hola"
+    },
+    {
+        "language": "English",
+        "hint_one": "Europe",
+        "hint_two": "Indo-European",
+        "hint_three": "Hello"
+    },
+    {
+        "language": "Hindi",
+        "hint_one": "Asia",
+        "hint_two": "Indo-European",
+        "hint_three": "Namaste"
+    },
+    {
+        "language": "Bengali",
+        "hint_one": "Asia",
+        "hint_two": "Indo-European",
+        "hint_three": "Salam"
+    },
+    {
+        "language": "Portuguese",
+        "hint_one": "Europe",
+        "hint_two": "Indo-European",
+        "hint_three": "Olá"
+    },
+    {
+        "language": "Russian",
+        "hint_one": "Europe/Asia",
+        "hint_two": "Indo-European",
+        "hint_three": "Zdravstvuyte"
+    },
+    {
+        "language": "Japanese",
+        "hint_one": "Asia",
+        "hint_two": "Japonic",
+        "hint_three": "Konnichiwa"
+    },
+    {
+        "language": "Western Punjabi",
+        "hint_one": "Asia",
+        "hint_two": "Indo-European",
+        "hint_three": "Sat sri akaal"
+    },
+    {
+        "language": "Marathi",
+        "hint_one": "Asia",
+        "hint_two": "Indo-European",
+        "hint_three": "Namaskar"
+    },
+    {
+        "language": "Telugu",
+        "hint_one": "Asia",
+        "hint_two": "Dravidian",
+        "hint_three": "Namaskaram"
+    },
+    {
+        "language": "Wu Chinese",
+        "hint_one": "Asia",
+        "hint_two": "Sino-Tibetan",
+        "hint_three": "Nong hao"
+    },
+    {
+        "language": "Turkish",
+        "hint_one": "Asia/Europe",
+        "hint_two": "Turkic",
+        "hint_three": "Merhaba"
+    },
+    {
+        "language": "Korean",
+        "hint_one": "Asia",
+        "hint_two": "Koreanic",
+        "hint_three": "Annyeonghaseyo"
+    },
+    {
+        "language": "French",
+        "hint_one": "Europe",
+        "hint_two": "Indo-European",
+        "hint_three": "Bonjour"
+    },
+    {
+        "language": "German",
+        "hint_one": "Europe",
+        "hint_two": "Indo-European",
+        "hint_three": "Guten Tag"
+    },
+    {
+        "language": "Vietnamese",
+        "hint_one": "Asia",
+        "hint_two": "Austroasiatic",
+        "hint_three": "Xin chào"
+    },
+    {
+        "language": "Tamil",
+        "hint_one": "Asia",
+        "hint_two": "Dravidian",
+    },
+    {'language': 'Hebrew',
+     'hint_one': 'Asia',
+     'hint_two': 'Afro-Asiatic',
+     'hint_three': 'shalom'},
+
+    {
+    'language': 'Czech',
+     'hint_one': 'Europe',
+     'hint_two': 'Indo-European', 'hint_three': 'ahoj'
+    },
+    {
+    'language':'Navajo',
+     'hint_one': 'North America',
+     'hint_two': 'Athabaskan',
+     'hint_three': "ya'at'eeh"
+    },
+    {
+    'language':'Inuit',
+     'hint_one': 'North America',
+     'hint_two': 'Eskimo-Aleut',
+     'hint_three': 'ᐃᓄᒃᑎᑐᑦ (inuktitut)'
+    },
+    {
+    'language': 'Kurdish',
+     'hint_one': 'Asia',
+     'hint_two': 'Indo-European',
+     'hint_three': 'salaw'
+    },
+    {
+    'language': 'Persian',
+     'hint_one': 'Asia',
+     'hint_two': 'Indo-European',
+     'hint_three': 'salam'
+    },
+    {
+    'language': 'Cherokee',
+     'hint_one': 'North America',
+     'hint_two': 'Iroquoian',
+     'hint_three': 'osiyo'
+    },
+    {
+    'language': 'Nahuatl',
+     'hint_one': 'North America',
+     'hint_two': 'Uto-Aztecan',
+     'hint_three': 'cualli'
+    },
+    {
+    'language': 'Indonesian',
+     'hint_one': 'Asia',
+     'hint_two': 'Austronesian',
+     'hint_three': 'salamat'
+    },
+    {
+    'language': 'Thai',
+     'hint_one': 'Asia',
+     'hint_two': 'Tai-Kadai',
+     'hint_three': 'sawatdii'
+    },
+    {
+    'language': 'Swahili',
+     'hint_one': 'Africa',
+     'hint_two': 'Bantu',
+     'hint_three': 'jambo'
+    },
+    {
+    'language':'Kinyarwanda',
+     'hint_one': 'Africa',
+     'hint_two': 'Bantu',
+     'hint_three': 'muraho'
+    },
+    {
+    'language': 'Italian',
+     'hint_one': 'Europe',
+     'hint_two': 'Indo-European',
+     'hint_three': 'ciao'
+    },
+    {
+    'language': 'Arabic',
+    'hint_one': 'Asia',
+    'hint_two': 'Afro-Asiatic',
+    'hint_three': 'marhaba'
+    },
+]
+
+language_data = [
+    {
+        "name": "English",
+        "consonants": 40,
+        "number of speakers": 1.27,
+        "number of vowels": 14
+    },
+    {
+        "name": "Dutch",
+        "consonants": 25,
+        "number of speakers": 0.24,
+        "number of vowels": 8
+    },
+    {
+        "name": "French",
+        "consonants": 31,
+        "number of speakers": 0.27,
+        "number of vowels": 16
+    },
+    {
+        "name": "German",
+        "consonants": 40,
+        "number of speakers": 0.13,
+        "number of vowels": 16
+    },
+    {
+        "name": "Italian",
+        "consonants": 25,
+        "number of speakers": 0.09,
+        "number of vowels": 7
+    },
+    {
+        "name": "Spanish",
+        "consonants": 24,
+        "number of speakers": 0.58,
+        "number of vowels": 5
+    },
+    {
+        "name": "Portuguese",
+        "consonants": 22,
+        "number of speakers": 0.22,
+        "number of vowels": 7
+    },
+    {
+        "name": "Modern Greek",
+        "consonants": 25,
+        "number of speakers": 0.03,
+        "number of vowels": 5
+    },
+    {
+        "name": "Mandarin Chinese",
+        "consonants": 23,
+        "number of speakers": 1.12,
+        "number of vowels": 9
+    },
+    {
+        "name": "Thai",
+        "consonants": 44,
+        "number of speakers": 0.09,
+        "number of vowels": 32
+    },
+    {
+        "name": "Vietnamese",
+        "consonants": 27,
+        "number of speakers": 0.09,
+        "number of vowels": 12
+    },
+    {
+        "name": "Indonesian",
+        "consonants": 18,
+        "number of speakers": 0.2,
+        "number of vowels": 6
+    },
+    {
+        "name": "Swahili",
+        "consonants": 24,
+        "number of speakers": 0.1,
+        "number of vowels": 5
+    },
+    {
+        "name": "Kinyarwanda",
+        "consonants": 27,
+        "number of speakers": 0.12,
+        "number of vowels": 5
+    },
+    {
+        "name": "Navajo",
+        "consonants": 34,
+        "number of speakers": 0.17,
+        "number of vowels": 5
+    },
+    {
+        "name": "Cherokee",
+        "consonants": 18,
+        "number of speakers": 0.022,
+        "number of vowels": 6
+    },
+    {
+        "name": "Inuit",
+        "consonants": 21,
+        "number of speakers": 0.01,
+        "number of vowels": 4
+    },
+    {
+        "name": "Arabic",
+        "consonants": 28,
+        "number of speakers": 4.49,
+        "number of vowels": 3
+    },
+    {
+        "name": "Arabic",
+        "consonants": 28,
+        "number of speakers": 422.2,
+        "number of vowels": 3
+    },
+    {
+        "name": "Hebrew",
+        "consonants": 22,
+        "number of speakers": 9.3,
+        "number of vowels": 5
+    },
+    {
+        "name": "Hindi",
+        "consonants": 34,
+        "number of speakers": 615,
+        "number of vowels": 11
+    },
+    {
+        "name": "Tamil",
+        "consonants": 18,
+        "number of speakers": 80,
+        "number of vowels": 12
+    },
+    {
+        "name": "Japanese",
+        "consonants": 14,
+        "number of speakers": 128.3,
+        "number of vowels": 5
+    },
+    {
+        "name": "Korean",
+        "consonants": 18,
+        "number of speakers": 77.2,
+        "number of vowels": 8
+    },
+    {
+        "name": "Russian",
+        "consonants": 35,
+        "number of speakers": 258.2,
+        "number of vowels": 5
+    },
+    {
+        "name": "Georgian",
+        "consonants": 28,
+        "number of speakers": 4.3,
+        "number of vowels": 5
+    },
+    {
+        "name": "Turkish",
+        "consonants": 21,
+        "number of speakers": 87.6,
+        "number of vowels": 8
+    },
+    {
+        "name": "Mongolian",
+        "consonants": 25,
+        "number of speakers": 6.8,
+        "number of vowels": 5
+    },
+    {
+        "name": "Persian",
+        "consonants": 23,
+        "number of speakers": 110,
+        "number of vowels": 6
+    },
+    {
+        "name": "Pashto",
+        "consonants": 24,
+        "number of speakers": 45,
+        "number of vowels": 5
+    },
+    {
+        "name": "Kurdish",
+        "consonants": 24,
+        "number of speakers": 30,
+        "number of vowels": 8
+    },
+    {
+        "name": "Cantonese",
+        "consonants": 20,
+        "number of speakers": 71.5,
+        "number of vowels": 6
+    }
+    ]
---
+++ language_game_project/main.py
@@ -0,0 +1,2 @@
+from data import language_data
+
